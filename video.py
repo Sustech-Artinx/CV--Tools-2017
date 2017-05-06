ca2899fc07dd80219f94b648ce06262a42d2aa38
OscarZhangF <transportation1996@vip.qq.com>
2017-05-06 20:16:09 +0800

    add file test

---
+++ video.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('blue.avi', fourcc, 20.0, (640, 480))
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret:
+        out.write(frame)
+        cv2.imshow('temp', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
